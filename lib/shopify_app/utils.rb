d7b652abd2190826d3046d1983fde4373719e017
Jon G <jon.g@shopify.com>
2019-09-06 11:24:25 -0400

    update api version in generators

--- lib/shopify_app/utils.rb
+++ lib/shopify_app/utils.rb
@@ -12,5 +12,20 @@ module ShopifyApp
       nil
     end
 
+    def self.fetch_known_api_versions
+      fetching_msg = "[ShopifyAPI::ApiVersion] Fetching known Admin API Versions from Shopify..."
+      Rails.logger.info(fetching_msg)
+      puts fetching_msg
+      ShopifyAPI::ApiVersion.fetch_known_versions
+
+      known_versions_msg = "[ShopifyAPI::ApiVersion] Known API Versions: #{ShopifyAPI::ApiVersion.versions.keys}"
+      Rails.logger.info(known_versions_msg)
+      puts known_versions_msg
+
+      rescue ActiveResource::ConnectionError
+        error_msg = "[ShopifyAPI::ApiVersion] Unable to fetch api_versions from Shopify"
+        logger.error(error_msg)
+        puts error_msg
+    end
   end
 end
